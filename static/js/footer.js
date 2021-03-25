18670d6cb6a735efff8514872dfc207b283670f5
Nepmia <64558289+Nepmia@users.noreply.github.com>
2021-03-25 14:35:33 +0100

    reworked the script now uses height() and is working properly

--- static/js/footer.js
+++ static/js/footer.js
@@ -1,7 +1,11 @@
 function footerPlacer() {
-    var page = $(".page-content");  
-    var footer_pos = page.position().top + page.outerHeight(true);
+    var page = $(".page-content").height();  
+    var footer = $(".foot").height();
+    var footer_pos = page - footer;
     $(".foot").css("top", footer_pos + "px");
+    console.log("footer_pos=" + footer_pos);
+    console.log("page_h=" + page);
+    console.log("footer_h=" + footer);
 }
 
 footerPlacer();
