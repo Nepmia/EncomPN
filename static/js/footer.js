605e0540f1d731a67be867e181c082a07edbbc09
Nepmia <64558289+Nepmia@users.noreply.github.com>
2021-03-25 15:03:22 +0100

    removed debug logs

--- static/js/footer.js
+++ static/js/footer.js
@@ -3,9 +3,6 @@ function footerPlacer() {
     var footer = $(".foot").height();
     var footer_pos = page - footer;
     $(".foot").css("top", footer_pos + "px");
-    console.log("footer_pos=" + footer_pos);
-    console.log("page_h=" + page);
-    console.log("footer_h=" + footer);
 }
 $(document).ready(() =>{
     footerPlacer();
