aa9bc3cda8a80308e4c9a92219694046a9bb9668
Nepmia <64558289+Nepmia@users.noreply.github.com>
2021-03-26 14:37:42 +0100

    Made enable side bar case in sidenav function

--- static/js/sidenav.js
+++ static/js/sidenav.js
@@ -1,5 +1,6 @@
 function sideNav (status){
     if (status == "on") {
-        
+        $(".sidenav").addClass("sideOn").removeClass("sideOff");
+        $(".sidenav-noInt").removeClass("none").addClass("visible");
     }
 }
