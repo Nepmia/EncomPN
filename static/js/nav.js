6bf0d3b18463cc5f74145e6bc76e16daa76313ef
Nepmia <64558289+Nepmia@users.noreply.github.com>
2021-03-26 20:05:12 +0100

    Pre-Made and testing more details in desc
    - Made basic nav scrip
    - Made scroll fx navbar (need testing)
    
    TODO: Make an hash powered navigation script.

---
+++ static/js/nav.js
@@ -0,0 +1,71 @@
+/* Page Navigation */
+const titles = {
+    home: "Home - EncomPN",
+    demo: "Demo - EncomPN",
+    map: "Relay Map - EncomPN"
+}
+
+$(document).ready(() => {
+    $(".pageChanger").click(function() {
+            var newPage = $(this).attr("page");
+            var newTitle = eval("titles." + newPage);
+            document.title = newTitle;
+            saved_scroll = 0;
+            contentLoad(newPage);
+            toggleNavbar("on");
+    });
+});
+function contentLoad(newPage){
+    $("footer").addClass("hidden");
+    setTimeout(() => {
+        $(".page-content").empty();
+        loadContent(newPage);
+    },300);
+};
+function loadContent(newPage){
+    var thisUrl = window.location.origin;
+    var newUrl = thisUrl + "/template/" + newPage;
+    $(".page-content").load( newUrl + " div.page-content");
+    $("footer").removeClass("hidden");
+}
+
+
+
+$(document).ready(() =>{
+    $(".content").addClass("c-on");
+});
+
+/* Navbar scroll fx */
+var saved_scroll = 0;
+$(document).ready(function() {
+    $(window).scroll(function() {
+          var new_scroll = $(window).scrollTop();
+          if (new_scroll > saved_scroll){
+              if ( new_scroll > 95){
+                toggleNavbar("off");
+                $(".navbar").addClass("off");  
+              } else {
+                  // pass
+              }
+            saved_scroll = new_scroll;
+          } else {
+            toggleNavbar("on");
+            $(".navbar").addClass("on");
+            saved_scroll = new_scroll;
+          }
+    });
+
+    $(".navbar").hover(function(){
+      toggleNavbar("on");
+    }, function(){
+      toggleNavbar("off");
+    });
+});
+
+function toggleNavbar(status){
+  if ( status == "on"){
+    $(".navbar").css("transform", "translateY(0px)");
+  } else {
+    $(".navbar").css("transform", "translateY(-50px)");
+  } 
+}
