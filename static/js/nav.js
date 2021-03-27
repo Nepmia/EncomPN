336200860c02f88626a336d30a33a41f453b2589
Nepmia <64558289+Nepmia@users.noreply.github.com>
2021-03-27 11:58:08 +0100

    Made basic working script

--- static/js/nav.js
+++ static/js/nav.js
@@ -3,19 +3,23 @@ const titles = {
     home: "Home - EncomPN",
     demo: "Demo - EncomPN",
     map: "Relay Map - EncomPN"
-}
+};
 
 $(document).ready(() => {
     $(".pageChanger").click(function() {
-            var newPage = $(this).attr("page");
-            var newTitle = eval("titles." + newPage);
-            document.title = newTitle;
-            saved_scroll = 0;
-            contentLoad(newPage);
-            toggleNavbar("on");
+        var newPage = $(this).attr("page");
+        $('html,body').animate({ scrollTop: 0 }, 'slow');
+        changePage(newPage);
+        contentUnload(newPage);
     });
 });
-function contentLoad(newPage){
+function changePage(newPage){
+    var newTitle = eval("titles." + newPage);
+    document.title = newTitle;
+    saved_scroll = 0;
+    window.location.hash = newPage;
+};
+function contentUnload(newPage){
     $("footer").addClass("hidden");
     setTimeout(() => {
         $(".page-content").empty();
@@ -25,15 +29,18 @@ function contentLoad(newPage){
 function loadContent(newPage){
     var thisUrl = window.location.origin;
     var newUrl = thisUrl + "/template/" + newPage;
-    $(".page-content").load( newUrl + " div.page-content");
+    // $(".page-content").load( newUrl + " div.page-content");
     $("footer").removeClass("hidden");
-}
+};
+function navCheck(){
+    var newPage = window.location.hash;
+    navSwitch(newPage)
+};
 
 
 
-$(document).ready(() =>{
-    $(".content").addClass("c-on");
-});
+window.onhashchange = navCheck;
+
 
 
 
