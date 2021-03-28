f9fea9e00d34bf1ef18b609709af77ba0e69406b
Nepmia <64558289+Nepmia@users.noreply.github.com>
2021-03-28 15:07:10 +0200

    Polished and finished animations for content and poping, now need to be real content and can be adapted to all others eContainers content

--- static/js/eContainer.js
+++ static/js/eContainer.js
@@ -5,9 +5,16 @@ function eContainerPop(){
         setTimeout(() =>{
             $(".ec-c").addClass("eC-c-off");
             setTimeout(() => {
-                $(".ec-c").removeClass("eC-c-off"); 
+                $(".ec-c").removeClass("eC-c-off");
+                eContentPop();  
             },50);   
         },100);    
     },1000);
-    
 };
+function eContentPop() {
+    $('.test').children().each(function(i){
+        setTimeout ( function(){
+            $('.ec-elt' + i).removeClass('ec-elt-off');
+          },i * 20);
+        });
+}
