0110fc1fab743ba22358419ef841949cbd203b6c
Nepmia <64558289+Nepmia@users.noreply.github.com>
2021-03-28 20:39:11 +0200

    Finished eBtn script so all eBtn are made and imported autmatically

--- static/js/eBtn.js
+++ static/js/eBtn.js
@@ -1,6 +1,7 @@
 function importer(){
-    $(".eBtn").each(function(){
-        var text = $(this).attr("text");
-        $(this).load("/template/eBtn.html")
+    $('.eBtn').each(function(){ // For loop of each eBtn classes 
+        var text = $(this).attr('text'); // get value of btn text   // \/ append eBtn template to this
+        $(this).append("<div class='frow center'><div class='eBtn-left frow center'><div class='eBtn-line-c2 fcol center'><div class='eBtn-line2'></div> </div><div class='eBtn-line-c1 fcol center'><div class='eBtn-line1 l1l'></div> </div></div><div class='eBtn-frame'><div class='eBtn-bg'><span id='eBtn' class='eBtn-text uB'>text</span></div></div><div class='eBtn-right frow center'><div class='eBtn-line-c1 fcol center'><div class='eBtn-line1 l1r'></div> </div><div class='eBtn-line-c2 fcol center'><div class='eBtn-line2'></div> </div></div></div>");
+        $(this).find("#eBtn").text(text); // Gives the good text to the btn
     });
 };
