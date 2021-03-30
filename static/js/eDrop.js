5e0151c34ba464d3c29da6acb923dd6fdf71ce5d
Nepmia <64558289+Nepmia@users.noreply.github.com>
2021-03-30 16:42:33 +0200

    Finished selections eDrop, now making attributes to use it with run

--- static/js/eDrop.js
+++ static/js/eDrop.js
@@ -21,17 +21,20 @@ const cities = [
     "Séoul",
     "Luxembourg"
 ]
+var from;
+var to;
+
 
 function cityImporter() {
     for (city_name in cities.sort()) {
-        $(".eDrop-ul").append("<li class='btrans eDrop-list-item uL eDrop-marg'>" + cities[city_name] + "</li>");
+        $(".eDrop-ul").append("<li class='btrans eDrop-list-item uL eDrop-marg' item='" + cities[city_name] + "'>" + cities[city_name] + "</li>");
     }
 }
 function closeDrop() {
     $(".eDrop-bg").css({"height": "50px","overflow-y": "hidden"}).removeClass("opened");
     $(".eDrop-bg").find("#eDrop").css("pointer-events", "none");
     if ($(".eDrop-bg").hasClass("noSelect")){
-        $(".eDrop-bg").animate({ scrollTop: 0 }, "slow", () => { // Rescroll up
+        $(".noSelect").animate({ scrollTop: 0 }, "slow", () => { // Rescroll up
             // pass
         });
     } else if ($(".eDrop-bg").hasClass("Selected")){
@@ -60,10 +63,11 @@ $(document).on("click", ".eDrop-ul", function(event){
 });
 
 $(document).on("click", ".eDrop-list-item", function(){
-    console.log("clique salope")
     var scrolled = $(this).position();
-    $(this).parent(".eDrop-bg").addClass("Selected").removeClass("noSelect");
-    $(".eDrop-bg").animate({ scrollTop: scrolled }, "slow", () => { // Rescroll up
-        // pass
+    console.log("clique salope" + scrolled.top);
+    console.log($(".opened").find(".eDrop-bg"));
+    $(".opened").addClass("Selected").removeClass("noSelect");
+    $(".opened").animate({ scrollTop: scrolled.top }, "slow", () => { // Rescroll up
+        closeDrop();
     });
 });
