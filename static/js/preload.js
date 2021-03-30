3400f9f887cb777b4e2a193566d5ba7202f03d90
Nepmia <64558289+Nepmia@users.noreply.github.com>
2021-03-30 11:57:50 +0200

    Fixed pages already loaded when preloading also fixed pages not loading when changing page

--- static/js/preload.js
+++ static/js/preload.js
@@ -3,8 +3,14 @@ window.onload = function() {
     $("video").on("ended", function(){
         footerPlacer();
         $(".preloader-table").addClass("hidden");
-        setTimeout(() => {
-            $(".preloader-table").addClass("none");
-        },200);
+        $(".eContainer").each(function(){
+            if ($(this).hasClass("eContainer-Scroller")) {
+                // pass
+            } else {
+                var rClass = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+                $(this).addClass(rClass);
+                eContainerPopScroll(rClass);
+            }
+        });
     });
   }
